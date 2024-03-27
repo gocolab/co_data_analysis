f5369aee14aded67a79d444d9a17aeabb364e662
Sanghun Oh <otter.oh@gmail.com>
2024-03-27 12:19:06 +0900

    add somethings

--- src/samples/JavaWithMysql.java
+++ src/samples/JavaWithMysql.java
@@ -1,3 +1,5 @@
+package samples;
+
 import java.sql.*;
 import java.util.HashMap;
 
--- src/samples/Sample.java
+++ src/samples/Sample.java
@@ -1,3 +1,5 @@
+package samples;
+
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Hello World!");
